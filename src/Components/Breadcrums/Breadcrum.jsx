02dcba82f40afd536b16f6ae6c34c4a636484b8e
Savindu_Rashmika <savindurashmika2000@gmail.com>
2024-06-20 21:04:56 +0530

    creating product part

---
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import './Breadcrum.css'
+import arro__icon from '../Assects/breadcrum_arrow.png'
+const Breadcrums = (props) => {
+
+  const {product}=props;
+  return (
+    <div className='bradcrum'>
+        HOME
+      
+    </div>
+  )
+}
+
+export default Breadcrums
